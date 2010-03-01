4cf55f405c26c11ee580c58ce48db570d9b7dd72
howthebodyworks <git@email.possumpalace.org>
2010-02-28 16:15:47 -0800

    move cruft/example JS out of the way

--- javascript/mongo-fulltext/_load.js
+++ javascript/mongo-fulltext/_load.js
@@ -30,6 +30,21 @@ files.forEach(
     }
 );
 
+// examples of storing functions for later use
+// from storefunc.js
+s = db.system.js;
+
+s.insert( { _id : "bar" , value : function( z ){ return 17 + z; } } );
+assert.eq( 22 , db.eval( "return bar(5);"  ) , "exec - 3 " );
+
+assert( s.getIndexKeys().length > 0 , "no indexes" );
+assert( s.getIndexKeys()[0]._id , "no _id index" );
+
+// check the "jstests" dir of the mongodb source for some exciting examples of 
+// stuff being done - such as mr*.js for mapreduce, and group* for
+// grouping
+
+
 
 // examples of storing functions for later use
 // from storefunc.js
