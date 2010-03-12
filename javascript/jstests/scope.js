40956dfd92f8b26168013b928636307aafceb3d6
howthebodyworks <git@email.possumpalace.org>
2010-03-11 20:44:01 -0600

    don't use fixtures in scope test

--- javascript/jstests/scope.js
+++ javascript/jstests/scope.js
@@ -3,23 +3,20 @@ var s = db.scope_test;
 s.drop();
 var j = db.system.js;
 
-var fixture = [
-    { _id : "scope_x" , value : 4 },
-    { _id : "scope_function_returning_closure" ,
+j.save({ _id : "scope_x" , value : 4 });
+j.save({ _id : "scope_function_returning_closure" ,
         value : function(){
             var scope_closure_var = 73;
             return function(){
                 return scope_closure_var;
             };
         }()
-    },
-];
+       });
 
-mft_util.load_records_from_list(fixture, 'system.js');
 assert.eq(2 , j.find({_id: /^scope_/}).count() , "setup functions" );
 assert.eq(db.eval("return scope_x;"), 4, "scope_x" );
 // the following test will raise an exception; stored functions do not retain their closures in MongoDB
 // assert.eq(db.eval("return scope_function_returning_closure();"), 73, "scope_function_returning_closure" );
-// However, this works find, sinc ethe variable will be looked up in glboal scope
+// However, this works find, since the variable will be looked up in glboal scope
 j.save({_id: "scope_closure_var", value: 74});
-assert.eq(db.eval("return scope_function_returning_closure();"), 74, "closue is global" );
+assert.eq(db.eval("return scope_function_returning_closure();"), 74, "closure is global" );
