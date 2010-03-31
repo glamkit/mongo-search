d6732b1bb7c1f08a5dcfd1fc9733dc507a089fd6
howthebodyworks <git@email.possumpalace.org>
2010-03-31 22:06:25 +1300

    remove spurious tests

--- javascript/mongo-fulltext/_load.js
+++ javascript/mongo-fulltext/_load.js
@@ -34,9 +34,6 @@ files.forEach(
 // from storefunc.js
 s = db.system.js;
 
-s.insert( { _id : "bar" , value : function( z ){ return 17 + z; } } );
-assert.eq( 22 , db.eval( "return bar(5);"  ) , "exec - 3 " );
-
 assert( s.getIndexKeys().length > 0 , "no indexes" );
 assert( s.getIndexKeys()[0]._id , "no _id index" );
 
