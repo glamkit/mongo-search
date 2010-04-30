b01b3dc4c90059ef524a69bbbe873a360a2759d0
howthebodyworks <git@email.possumpalace.org>
2010-04-30 16:41:20 +0800

    squash me. ranking tests in the repo, although the results are now monstrous bad and it seems i broke something in creating them - why are all values 0?

---
+++ javascript/jstests/ranking_works.js
@@ -0,0 +1,34 @@
+load('mongo-fulltext/_load.js');
+var s = db.ranking_works;
+s.drop();
+var full_vector_norm = false;
+
+
+var fixture = [
+    { "_id" : 1, "title" : "fish", "content" : "groupers like John Dory" },
+    { "_id" : 2, "title" : "dogs", "content" : "whippets kick mongrels" },
+    { "_id" : 3, "title" : "dogs & fish", "content" : "whippets kick groupers" }
+];
+var conf = db.fulltext_config;
+conf.insert({'collection_name' : 'ranking_works', 'fields': {'title': 5, 'content': 1}, 'params': {'full_vector_norm': full_vector_norm}});
+
+
+mft.get('util').load_records_from_list(fixture, 'ranking_works');
+var search = mft.get('search');
+search.mapReduceIndex('ranking_works');
+
+var result ;
+
+
+// TODO: add index on collection name (should we have an _id attribute too?)
+result = search.mapReduceSearch('ranking_works', 'fish').toArray();
+
+// print(result, "Search result for 'fish'");
+
+assert.eq(result, [{ "_id" : 1, "value" : 0 }, { "_id" : 3, "value" : 0 }]);
+
+result = search.mapReduceSearch('ranking_works', 'Dory').toArray();
+
+// print(result, "Search result for 'Dory'");
+
+assert.eq(result, [ { "_id" : 1, "value" : 0 }]);
