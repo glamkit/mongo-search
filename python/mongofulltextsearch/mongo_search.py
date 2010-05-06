568960da1b98a6b82342f3a3a55c13e8569c0934
howthebodyworks <git@email.possumpalace.org>
2010-05-06 18:37:42 +0800

    stubbing out the mapreduce fns

--- python/mongofulltextsearch/mongo_search.py
+++ python/mongofulltextsearch/mongo_search.py
@@ -1,3 +1,20 @@
 # −*− coding: UTF−8 −*−
+"""
+This is the python wrapper to the mongo full text search javascript.
+As such it has to implement all the mapreduce invocations in the javascript
+library, plus anything else that we do not wish to block the server upon
+executing. That is:
+
+search.mapReduceIndex
+search.mapReduceSearch
+search.mapReduceNiceSearch
+search.stemAndTokenize (and thus search.stem and search.tokenizeBasic )
+
+Optional:
+search.processQueryString
+search.encodeQueryString
+"""
 import pymongo
 
+def index():
+    pass
