3b423bfcee420a2ecef586aea20eed7bf8439532
howthebodyworks <git@email.possumpalace.org>
2010-05-06 14:29:17 +0800

    let's test this baby

---
+++ python/tests/utils.py
@@ -0,0 +1,31 @@
+# −*− coding: UTF−8 −*−
+from nose.tools import nottest, assert_equals
+
+@nottest
+def clear_db():
+    from collection.models import get_db
+    db = get_db()
+    db.connection().drop_database(db)
+    db = get_db(flush_cached=True)
+
+def key_comparison(dict_a, dict_b, key):
+    val_a, val_b = dict_a.get(key), dict_b.get(key)
+    if hasattr(val_a, 'to_dict'): val_a = val_a.to_dict()
+    if hasattr(val_b, 'to_dict'): val_b = val_b.to_dict()
+    if val_a != val_b:
+        raise AssertionError(
+          "dict unequal for key %s. (%s != %s)" % (key, str(val_a), str(val_b))
+        )
+
+def dict_comparer(dict_a, dict_b):
+    """
+    yield keywise tests for dict equality, making it easy to see what is going on
+    """
+    if not hasattr(dict_a, 'keys'):
+        raise AssertionError("left operand doesn't look like a dict")
+    if not hasattr(dict_b, 'keys'):
+        raise AssertionError("right operand doesn't look like a dict")
+    for key in dict_a.keys():
+        yield key_comparison, dict_a, dict_b, key
+    keys_a, keys_b = dict_a.keys(), dict_b.keys()
+    keys_a.sort(); keys_b.sort()
