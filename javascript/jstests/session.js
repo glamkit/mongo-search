799bac94d647c15cb4e762df514812a0eeaba2e8
howthebodyworks <git@email.possumpalace.org>
2010-05-06 13:00:55 +0800

    tidying up session tests to at least be valid JS

--- javascript/jstests/session.js
+++ javascript/jstests/session.js
@@ -4,24 +4,24 @@
 // *which* we can now do - see assert.parallelTests
 
 load('mongo-fulltext/_load.js');
-var s = db.index_creation;
-s.drop();
-// the following lines raises asn error that we can't confirm using
+
+// the following lines raise an error that we can't confirm using
 // assert.throws, since that messes with method binding
 // Take-home message: scoping with var doesn't work
 // db.eval("var session_server_scoped_test_magic_number = 593;")
 db.eval("session_server_unscoped_test_magic_number = 275;");
 assert.eq(
   db.eval("return session_server_unscoped_test_magic_number;"),
-  593
+  275
 );
 session_client_object_scope_constructor = function() {};
-session_client_object_scope = new session_client_object_scope_constructor();
-session_client_object_scope.foo = 'own_property';
 session_client_object_scope_constructor.prototype = {
   bar: 'prototype_property',
   baz: function() {return 'prototype_method';}
 };
+session_client_object_scope = new session_client_object_scope_constructor();
+session_client_object_scope.foo = 'own_property';
+
 assert.eq(
   session_client_object_scope.foo,
   'own_property'
@@ -34,6 +34,7 @@ assert.eq(
   session_client_object_scope.baz(),
   'prototype_method'
 );
+
 db.eval("session_server_object_scope_constructor = function() {};");
 db.eval("session_server_object_scope_constructor.prototype = {" +
   "bar: 'prototype_property'," +
@@ -45,9 +46,11 @@ assert.eq(
   db.eval("return session_server_object_scope.foo;"),
   'own_property'
 );
-assert.eq(
-  db.eval("return session_server_object_scope.bar;"),
-  'prototype_property'
+assert(
+    //if prototypes were not pruned this would be what we expect
+    // db.eval("session_server_object_scope.bar;") === 'prototype_property'
+    db.eval("typeOf session_server_object_scope.bar;") === 'undefined',
+    "prototype properties are missing"
 );
 assert.eq(
   db.eval("return session_server_object_scope.baz();"),
