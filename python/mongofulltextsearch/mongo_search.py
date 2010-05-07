89db9077f910c48e5a28dc89ae844bfd0c3ad4aa
howthebodyworks <git@email.possumpalace.org>
2010-05-07 14:29:35 +0800

    more function stubbing

--- python/mongofulltextsearch/mongo_search.py
+++ python/mongofulltextsearch/mongo_search.py
@@ -16,5 +16,23 @@ search.encodeQueryString
 """
 import pymongo
 
-def index():
+def map_reduce_index(coll_name):
+    db.things.map_reduce(map, reduce, full_response=True)
+
+def map_reduce_search(coll_name, search_query_string):
+    pass
+
+def map_reduce_nice_search_by_query(coll_name, search_coll_name, query_obj):
+    pass
+
+def map_reduce_nice_search_by_ids(coll_name, search_coll_name, id_list):
+    pass
+    
+def stem_and_tokenize(phrase):
+    pass
+
+def stem(word):
+    pass
+
+def tokenize(phrase):
     pass
---
+++ python/mongofulltextsearch/util.py
@@ -0,0 +1,50 @@
+"""
+This is the python wrapper to the mongo full text search javascript
+functions.
+
+This mirrors the javascript _utils.js in that it provide serverside
+script updating and other miscellaneous infrastructure.
+"""
+
+def load_records_from_list(list, collection):
+    pass
+
+def load_server_functions(code, connection):
+    """
+    It woudl be nice to parse the JS lib in some way to give us a
+    semblance of control, and also to use the IMHO superior code
+    accessors in pymongo.database.Database.system_js. but srsly,
+    let's jsut run the perfectly good js loader we already have
+    """
+    pass
+
+def get_js_file_contents(rel_path):
+    """
+    special python access for the javascript files which are stashed
+    in some inconvenient place
+    """
+    pass
+    
+# //load an array of records into the specified collection
+# util.load_records_from_list = function(record_list, coll_name) {
+#     if (typeof coll_name == 'undefined') {
+#         coll_name = 'test';
+#     }
+# 
+#     record_list.forEach(
+#         function(record) {
+#             db[coll_name].save(record);
+#         }
+#     );
+# };
+# 
+# util.init_test_collection = function(coll_name) {
+#     if (typeof coll_name == 'undefined') {
+#         coll_name = 'test';
+#     }
+#     db[coll_name].drop();
+# };
+# 
+# util.load_server_functions = function() {
+#     load('mongo-fulltext/_load.js');
+# };
