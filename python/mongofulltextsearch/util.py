12e035293b7a281fef426c682ea3584637396de1
howthebodyworks <git@email.possumpalace.org>
2010-05-10 09:19:06 +1000

    js loader function now goes

--- python/mongofulltextsearch/util.py
+++ python/mongofulltextsearch/util.py
@@ -5,6 +5,7 @@ functions.
 This mirrors the javascript _utils.js in that it provide serverside
 script updating and other miscellaneous infrastructure.
 """
+js_path = ''
 
 def load_records_from_list(list, collection):
     pass
@@ -23,7 +24,14 @@ def get_js_file_contents(rel_path):
     special python access for the javascript files which are stashed
     in some inconvenient place
     """
-    pass
+    global js_path
+    if not js_path:
+        import path
+        here = path.path(__file__)
+        js_path = here.abspath().parent.parent.parent/'javascript'/'mongo-fulltext'
+    js_file = js_path/rel_path
+    return js_file.text('utf8')
+    
     
 # //load an array of records into the specified collection
 # util.load_records_from_list = function(record_list, coll_name) {
