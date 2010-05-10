53574ca0ba2f6d9ce30e526ca1f01627bef3d8e3
howthebodyworks <git@email.possumpalace.org>
2010-05-10 16:32:34 +1000

    we can now load javascript functions en masse from the python client

--- python/mongofulltextsearch/util.py
+++ python/mongofulltextsearch/util.py
@@ -5,31 +5,84 @@ functions.
 This mirrors the javascript _utils.js in that it provide serverside
 script updating and other miscellaneous infrastructure.
 """
-js_path = ''
+_js_root = ''
 
 def load_records_from_list(list, collection):
     pass
 
-def load_server_functions(code, connection):
+def get_database(**mongo_params):
     """
-    It woudl be nice to parse the JS lib in some way to give us a
+    get a mongo connection, falling back to the settings in a
+    settings file
+    """
+    from pymongo import Connection
+    defaults = {
+        'MONGO_HOST': 'localhost',
+        'MONGO_PORT': 27017,
+        'MONGO_DB': 'test',
+    }
+    try:
+        import settings
+        for k in defaults.keys():
+            defaults[k]=getattr(settings, k, defaults[k])
+    except ImportError:
+        pass
+    connection = Connection(
+            defaults['MONGO_HOST'],
+            defaults['MONGO_PORT']
+        )
+    return connection[defaults['MONGO_DB']]
+    
+def load_all_server_functions(database=None):
+    """
+    It would be nice to parse the JS lib in some way to give us a
     semblance of control, and also to use the IMHO superior code
     accessors in pymongo.database.Database.system_js. but srsly,
-    let's jsut run the perfectly good js loader we already have
+    let's just run the perfectly good js loader we already have
     """
-    pass
+    return exec_js_from_file(
+      'mongo-fulltext/_load.js',
+      database=database)
 
-def get_js_file_contents(rel_path):
+def get_js_root():
+    global _js_root
+    if not _js_root:
+        import path
+        here = path.path(__file__).abspath()
+        _js_root = here.parent.parent.parent/'javascript'
+    return _js_root
+
+def exec_js_from_file(relative_script_path, database=None):
+    """
+    Given a js path, run the javascript in it against the given
+    mongod database instance
+    How does this work with "load"?
+    """
+    import subprocess
+    if database is None: database = get_database()
+    js_root = get_js_root()
+    db_name = database.name
+    host = database.connection.HOST
+    port = database.connection.PORT
+    proc = subprocess.Popen(
+      [
+        'mongo',
+        host + ":" + str(port) + "/" + db_name, 
+        relative_script_path
+      ], cwd=str(js_root),
+      stdout = subprocess.PIPE,
+      stderr = subprocess.STDOUT
+    )
+    proc.wait()
+    return proc.returncode, proc.stdout.read()
+    
+def get_js_file_contents(relative_script_path):
     """
     special python access for the javascript files which are stashed
     in some inconvenient place
     """
-    global js_path
-    if not js_path:
-        import path
-        here = path.path(__file__)
-        js_path = here.abspath().parent.parent.parent/'javascript'/'mongo-fulltext'
-    js_file = js_path/rel_path
+    # /'mongo-fulltext'
+    js_file = get_js_root()/relative_script_path
     return js_file.text('utf8')
     
     
